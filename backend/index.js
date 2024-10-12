f6447e6bfe9f336f564fb069e0518c2657ab3276
Anish Sahoo <anishsahoo2005@gmail.com>
2024-10-12 18:18:35 -0500

    update port

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json({ limit: "1kb" }));
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 // Set up mongoose
 mongoose

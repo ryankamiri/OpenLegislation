e1e611221f8dddbcd5f42208b56357cc24d8f250
Smyan Sengupta <senguptasmyan@gmail.com>
2024-10-12 11:31:47 -0400

    push

--- open-legislation-front/src/Header.jsx
+++ open-legislation-front/src/Header.jsx
@@ -1,20 +1,17 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import SearchBar from './SearchBar';
-import { FaBookOpen } from "react-icons/fa";
 
 function Header() {
     return (
-        <div
-    className={`w-full text-center flex justify-between px-4 h-24 md:h-16 items-center`}>
-        <div className="flex justify-between w-full items-center">
-            <div className="items-center"></div>
-                <a href='./' className="hover:text-gray-300">OpenLegislation</a>
+        <header class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+            <div class="md:w-1/2 mt-8 md:mt-0 text-center md:text-left">
+                <a href='./' className="text-black hover:text-gray-600 transition-colors duration-200">OpenLegislation</a>
                 <div className='m-2 p-6'>
                     <SearchBar />
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
 

0808f18bb82fe8ed4186a9d97d0d4b4778b85851
Shishir Pokhrel <shishirpokhrel@Shishirs-MacBook-Pro.local>
2024-10-12 16:53:14 -0400

    add landing page and result page

--- open-legislation-front/src/LandingPage/LandingPage.jsx
+++ open-legislation-front/src/LandingPage/LandingPage.jsx
@@ -1,26 +1,43 @@
 import { FaBookOpen } from "react-icons/fa";
-import MainPage from "../main-page/MainPage";
 import { useNavigate } from "react-router-dom";
 
 export default function LandingPage() {
-
     const navigate = useNavigate();
 
-    return(
-        <div className='flex h-full bg-gray-100'>
-            <h1><FaBookOpen /> Open Legislation</h1>
-            <h2>How it works</h2>
-            <ol>
-                <li>Search for a bill or issue in Congress you are interested in</li>
-                <li>You will be given a list in chronological order</li>
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-6">
+            <div className="flex items-center text-4xl font-bold text-gray-800 mb-36">
+                <FaBookOpen className="mr-2 text-red-500" /> 
+
+                <div className="mx-5">Open Legislation</div>
+
+                <FaBookOpen className=" text-blue-500" />
+            </div>
+            
+            <div className="text-lg font-semibold text-gray-700 mb-4">
+                How it works
+                
+            </div>
+
+            <ol className="list-decimal list-inside text-left text-gray-700 space-y-2 mb-8 max-w-lg">
+                <li>Search keywords for the Congressional Bills.</li>
                 <li>Click on a bill to read more about it</li>
                 <li>Instead of reading through hundreds of pages, we will summarize it for you!</li>
                 <li>Save time and enjoy learning about these bills without having to read through legal jargon</li>
             </ol>   
-
-            
-            <button onClick={() => navigate('/results')}>Get started!</button>
-
+            <div>
+            <button 
+                onClick={() => navigate('/results')}
+                className="bg-red-500 text-white py-2 px-4  hover:bg-red-400 transition duration-300 rounded-tl-lg rounded-bl-lg"
+                >&nbsp;&nbsp;  Get &nbsp; 
+            </button>
+            <button 
+                onClick={() => navigate('/results')} 
+                className="bg-blue-500 text-white py-2 px-4  hover:bg-blue-400 transition duration-300 rounded-tr-lg rounded-br-lg"
+            >
+             Started!
+            </button>
+            </div>
         </div>
-    )
+    );
 }
--- open-legislation-front/src/ResultPage/ResultPage.jsx
+++ open-legislation-front/src/ResultPage/ResultPage.jsx
@@ -1,43 +1,159 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+// import React from "react";
 
-export default function ResultPage() {
-  const [bills, setBills] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+function Results() {
 
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        setBills(response.data); 
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setError('Error fetching posts');
-        setLoading(false);
-      });
-  }, []);
+  const jasonData = {
+    "bill": {
+        "latestAction": {
+            "actionDate": "2024-09-27T00:00:00.000Z",
+            "text": "Held at the desk."
+        },
+        "_id": "670ad88d8dc7cc1af018b4eb",
+        "billId": 1570,
+        "title": "Bottles and Breastfeeding Equipment Screening Enhancement Act",
+        "congressId": 118,
+        "originChamber": "Senate",
+        "updateDate": "2024-09-28T00:00:00.000Z",
+        "latestStage": "President",
+        "sponsor": {
+            "bioguideId": "D000622",
+            "firstName": "Tammy",
+            "fullName": "Sen. Duckworth, Tammy [D-IL]",
+            "isByRequest": "N",
+            "lastName": "Duckworth",
+            "party": "D",
+            "state": "IL",
+            "url": "https://api.congress.gov/v3/member/D000622?format=json"
+        },
+        "cosponsors": [
+            {
+                "bioguideId": "D000618",
+                "firstName": "Steve",
+                "fullName": "Sen. Daines, Steve [R-MT]",
+                "isOriginalCosponsor": true,
+                "lastName": "Daines",
+                "party": "R",
+                "sponsorshipDate": "2023-05-11",
+                "state": "MT",
+                "url": "https://api.congress.gov/v3/member/D000618?format=json"
+            },
+            {
+                "bioguideId": "H001042",
+                "firstName": "Mazie",
+                "fullName": "Sen. Hirono, Mazie K. [D-HI]",
+                "isOriginalCosponsor": true,
+                "lastName": "Hirono",
+                "middleName": "K.",
+                "party": "D",
+                "sponsorshipDate": "2023-05-11",
+                "state": "HI",
+                "url": "https://api.congress.gov/v3/member/H001042?format=json"
+            },
+            {
+                "bioguideId": "C001098",
+                "firstName": "Ted",
+                "fullName": "Sen. Cruz, Ted [R-TX]",
+                "isOriginalCosponsor": false,
+                "lastName": "Cruz",
+                "party": "R",
+                "sponsorshipDate": "2024-01-30",
+                "state": "TX",
+                "url": "https://api.congress.gov/v3/member/C001098?format=json"
+            }
+        ],
+        "billUrl": "https://api.congress.gov/v3/bill/118/s/1570?format=json",
+        "textUrl": "https://www.congress.gov/118/bills/s1570/BILLS-118s1570es.htm",
+        "createdAt": "2024-10-12T20:14:05.909Z",
+        "updatedAt": "2024-10-12T20:14:05.909Z",
+        "__v": 0
+    },
+    "analysis": {
+        "summary": "The Bottles and Breastfeeding Equipment Screening Enhancement Act mandates the TSA and private security personnel to ensure hygienic handling of breast milk and baby formula during aviation security screening. The TSA must develop guidance to minimize contamination risks and conduct audits to assess compliance and the effectiveness of screening technologies.",
+        "potentialImpact": "This bill improves the safety and hygiene of breast milk and baby formula during air travel. It could reduce health risks for infants and increase comfort for breastfeeding mothers. By establishing clear protocols, it may also streamline the screening process, benefiting both families and security personnel.",
+        "impactedGroups": "Mothers breastfeeding during air travel, infants consuming breast milk or baby formula, Transportation Security Administration personnel, and private security screening companies.",
+        "pros": "The bill enhances safety measures for infants by ensuring hygienic handling of essential food items like breast milk. It supports breastfeeding mothers during travel, promoting maternal health. Additionally, developing standardized guidelines may improve the overall efficiency of security checks, easing the stress on families while traveling.",
+        "cons": "Implementing the guidelines may create additional work for TSA agents and private screening companies, potentially leading to longer wait times. There may be challenges in training staff to adhere to these new standards. It's also possible that the bill could encounter resistance from security personnel due to workflow changes."
+    }
+}
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
+const billInfo = jasonData.bill
+const billAnalysor = jasonData.analysis
 
   return (
     <div>
-      <h1>Posts</h1>
-      <ul>
-        {bills.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>Bill Information</h1>
+
+      {/* Displaying simple key-value pairs */}
+      <div>{ billInfo.billId}: {billInfo.title}</div>
+      
+      <div>Bill Sponsor: {billInfo.sponsor.fullName}</div>
+      
+      <div>Bill Co-sponsors: {billInfo.cosponsors.map((cosponsor, index) =>(
+        <li key={index}>{cosponsor.fullName}</li>
+      ))}</div>
+
+      <div>Stage: The bill is currently at {billInfo.latestStage}, since {billInfo.updateDate}</div>
+
+      <div>Summary</div>
+      <div>{billAnalysor.summary}</div>
+
+      <div>Potential Impacts</div>
+      <div>{billAnalysor.potentialImpact}</div>
+      
+      <div>Most impacted</div>
+      <div>{billAnalysor.impactedGroups}</div>
+
+      <div>
+        <div>Pros</div>
+        <div>{billAnalysor.pros}</div>
+
+        <div>Cons</div>
+        <div>{billAnalysor.cons}</div>
+      </div>
     </div>
   );
 };
+
+
+
+//   const [bills, setBills] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+    // useEffect(() => {
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //   .then((response) => {
+    //     setBills(response.data); 
+    //     setLoading(false);
+    //   })
+    //   .catch((error) => {
+    //     console.error('Error fetching data:', error);
+    //     setError('Error fetching posts');
+    //     setLoading(false);
+    //   });
+//   }, []);
+
+//   if (loading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   if (error) {
+//     return <p>{error}</p>;
+//   }
+
+//   return (
+//     <div>
+//       <h1>Posts</h1>
+//       <ul>
+//         {bills.map((post) => (
+//           <li key={post.id}>
+//             <h2>{post.title}</h2>
+//             <p>{post.body}</p>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+
+
+
+export default Results

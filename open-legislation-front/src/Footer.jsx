3edb8d484025da65a2115da91b81fa0eab63b725
Smyan Sengupta <senguptasmyan@gmail.com>
2024-10-12 07:27:53 -0400

    Frontend setup

---
+++ open-legislation-front/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function Footer() {
+    return (
+        <footer>
+            You have reached the bottom!
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ open-legislation-front/src/Header.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function Header() {
+    return (
+        <header>
+            <h1>
+                <a href='./'>OpenLegislation</a>
+            </h1>
+        </header>
+    )
+}
+
+export default Header;

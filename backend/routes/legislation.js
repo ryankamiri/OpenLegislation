0995e5cc785ce849795db993c1647994ef99f0d5
Ryan <44690200+ryankamiri@users.noreply.github.com>
2024-10-12 13:48:31 -0400

    Updated search API

--- backend/routes/legislation.js
+++ backend/routes/legislation.js
@@ -5,17 +5,62 @@ import gpt from "../gpt/gpt.js";
 
 const router = Router();
 
+const NUM_CANDIDATES = 10;
+const LIMIT = 10;
+
 router.get("/search", async (req, res) => {
   try {
-    // q: query, date: bills after x date, party: political party of sponsor, status: bill status
-    const { q, date, party, status } = req.query;
+    // q: query, date: bills after x date, party: political party of sponsor, stage: bill status
+    const { q, date, party, stage } = req.query;
     if (!q) {
       return res.status(400).json({ err: "Query not provided." });
     }
 
     // Make vector search
+    const embeddingQ = await gpt.createEmbedding(q);
 
-    return res.json({});
+    const agg = [
+      {
+        '$vectorSearch': {
+          'index': 'vector_index',
+          'path': 'title_vector',
+          'queryVector': embeddingQ,
+          "numCandidates": NUM_CANDIDATES,
+          "limit": LIMIT
+        }
+    }];
+
+    const match = {};
+
+    if (date) {
+      // Process date
+      const parsedDate = Date.parse(date);
+      if (parsedDate) {
+        // If we have a valid date
+        match.updateDate = { '$gte': parsedDate };
+      }
+    }
+    
+    if (party) {
+      match['sponsor.party'] = party;
+    }
+    
+    if (stage) {
+      match.latestStage = stage;
+    }
+
+    if (Object.keys(match).length > 0) {
+      agg.unshift({
+        '$match': match
+      })
+    }
+
+    const result = await Bill.aggregate(agg);
+    for (let i = 0; i < result.length; i++) {
+      delete result[i].title_vector;
+    }
+
+    return res.json(cleanResult);
   } catch (err) {
     return res.status(500).json({ err: err.message });
   }

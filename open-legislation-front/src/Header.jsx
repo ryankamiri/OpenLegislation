8adf521b92c9067e1a307a21d96a8c622bb7749f
Smyan Sengupta <senguptasmyan@gmail.com>
2024-10-12 23:14:57 -0400

    styling

--- open-legislation-front/src/Header.jsx
+++ open-legislation-front/src/Header.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 function Header() {
   return (
-    <header className="flex font-bold text-lg text-center bg-blue-500 flex-auto p-4">
+    <header className="flex font-bold text-lg text-center bg-gradient-to-r from-blue-500 to-red-500 flex-auto p-4">
       <a
         href="/"
         className="text-white hover:text-gray-300 transition-colors duration-200"

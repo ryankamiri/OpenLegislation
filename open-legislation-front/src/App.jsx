77c88c88e1f3a8ae3e0032f07850d5c52a9cc983
Shishir Pokhrel <shishirpokhrel@Shishirs-MacBook-Pro.local>
2024-10-12 20:20:07 -0400

    Add pie chart

--- open-legislation-front/src/App.jsx
+++ open-legislation-front/src/App.jsx
@@ -5,7 +5,9 @@ import Footer from './Footer'
 import MainPage from './main-page/MainPage'
 import LandingPage from './LandingPage/LandingPage'
 import Results from './ResultPage/Results'
+import PieChart from './ResultPage/PieChart'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
@@ -18,6 +20,7 @@ function App() {
               <Route path="/" exact element={<LandingPage />} />
               <Route path="/home" exact element={<MainPage />} />
               <Route path="/results" exact element={<Results />} />
+              <Route path="/piechart" exact element={<PieChart />} />
           </Routes>
           <Footer />
         </div>
